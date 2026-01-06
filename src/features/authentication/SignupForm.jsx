c7910e48c5aaabc9c6c6b2f3c238dd758b047afa
Igor Klusek <igorklusek@monogo-001s-macbook-pro-2.home>
2026-01-06 14:04:46 +0100

    TWO-14-Allow user to change data and upload images

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -104,6 +104,7 @@ function SignupForm() {
           variation="secondary"
           type="reset"
           disabled={isLoading}
+          onClick={reset}
         >
           Cancel
         </Button>
