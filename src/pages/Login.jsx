f33240ad21ff061ece679ff736e899f68b3beda7
Igor Klusek <igorklusek@monogo-001s-macbook-pro-2.home>
2026-01-06 12:59:28 +0100

    User update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import LoginForm from '../features/authentication/LoginForm';
 import Heading from '../ui/Heading';
 import Logo from '../ui/Logo';
+import CabinTable from '../features/cabins/CabinTable';
 
 const LoginLayout = styled.main`
   min-height: 100vh;
@@ -19,6 +20,8 @@ function Login() {
       <Logo />
       <Heading as="h3">Log in to your account</Heading>
       <LoginForm />
+
+      <CabinTable />
     </LoginLayout>
   );
 }
